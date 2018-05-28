d7a201d3a3b3c1e8b28689115f19064fb7a32bc2
Maciej Suchocki <m.suchocki96@gmail.com>
2018-05-28 23:36:45 +0200

    Main Class and JavaWorker main implementation added

---
+++ src/MapReduceJavaWorkerProgram.java
@@ -0,0 +1,25 @@
+import MapReduce.Thrift.JavaWorker.JavaWorker;
+import Multicast.MulticastReceiver;
+
+/**
+ * Created by suchy on 28.05.2018.
+ */
+public class MapReduceJavaWorkerProgram {
+    public static void main(String[] args) {
+
+        System.out.println("MapReduceJavaWorkerProgram started");
+
+        MulticastReceiver multicastReceiver = new MulticastReceiver();
+        System.out.println("MulticastReceiver created");
+
+        multicastReceiver.run();
+
+        // run worker
+        if(multicastReceiver.isMasterReady()){
+            JavaWorker worker = new JavaWorker();
+            worker.main();
+        }
+
+
+    }
+}
