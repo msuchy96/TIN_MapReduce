87341778ae35a7ac8017bbe6a64845395afb0c54
Maciej Suchocki <m.suchocki96@gmail.com>
2018-06-05 19:24:27 +0200

    catching exceptions in handler not in python runner

--- src/PythonPoCClient/PythonRunner.java
+++ src/PythonPoCClient/PythonRunner.java
@@ -29,91 +29,71 @@ public class PythonRunner {
         this.pythonPath = pythonPath;
     }
 
-    public void map(DataSyncWrapper dataSyncWrapper){
+    public void map(DataSyncWrapper dataSyncWrapper) throws FileNotFoundException,IOException, InterruptedException{
 
         //zakladamy dla uproszczenia, ze:
         //wywolujemy skrypt pythonowy, ktory czyta z wejscia standardowego
         //na wejscie standardowe MY wrzucamy dane z dataFile
         //a wyniki ma wypluwac na stdout w postaci KLUCZ=>WARTOSC, gdzie wartosc musi dac sie sparsowac na inta, klucz dowolny(string)
 
-        try {
-            Process process = createProcess(pythonMapPath);
 
-            OutputStream stdin = process.getOutputStream(); // The Process OuputStream (our point of view) is the STDIN from the process point of view
+        Process process = createProcess(pythonMapPath);
+
+        OutputStream stdin = process.getOutputStream(); // The Process OuputStream (our point of view) is the STDIN from the process point of view
+        InputStream stdout = process.getInputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+        BufferedReader br = new BufferedReader(new FileReader(dataFilePath));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+            writer.write(line);
+            writer.newLine();
+        }
+
+        writer.close();
+        process.waitFor();
+
+        while(reader.ready()) {
+            String resultPair[] = reader.readLine().split("=>");
+            System.out.println("Pair result from map: " + resultPair[0]+"=>"+resultPair[1]);
+            // to avoid sync problem with end of the
+            synchronized (dataSyncWrapper){
+                dataSyncWrapper.putInRegisterWorkersQueue(new Pair<>(resultPair[0],Integer.valueOf(resultPair[1])));
+                if(!reader.ready()){
+                    dataSyncWrapper.setEndOfRegisterWorkersQueue(true);
+                }
+                dataSyncWrapper.endOfAction(true);
+            }
+        }
+    }
+
+    public void reduce(DataSyncWrapper dataSyncWrapper) throws IOException, InterruptedException{
+
+        for(String key: dataSyncWrapper.getMyKeyValuesMap().keySet()){
+            Process process = createProcess(pythonReducePath);
+
+            OutputStream stdin = process.getOutputStream();
             InputStream stdout = process.getInputStream();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
 
-            BufferedReader br = new BufferedReader(new FileReader(dataFilePath));
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                writer.write(line);
+            for(Integer value: dataSyncWrapper.getMyKeyValuesMap().get(key)){
+                writer.write(key+"=>"+String.valueOf(value));
                 writer.newLine();
             }
-
             writer.close();
             process.waitFor();
 
-            while(reader.ready()) {
-                String resultPair[] = reader.readLine().split("=>");
-                System.out.println("Pair result from map: " + resultPair[0]+"=>"+resultPair[1]);
+            while(reader.ready()){
+                String result = reader.readLine();
                 // to avoid sync problem with end of the
-                synchronized (dataSyncWrapper){
-                    dataSyncWrapper.putInRegisterWorkersQueue(new Pair<>(resultPair[0],Integer.valueOf(resultPair[1])));
-                    if(!reader.ready()){
-                        dataSyncWrapper.setEndOfRegisterWorkersQueue(true);
-                    }
-                    dataSyncWrapper.endOfAction(true);
-                }
+                dataSyncWrapper.addToResultList(key,result);
             }
-
-        } catch(FileNotFoundException e){
-            System.out.println("File was not found in PythonRunner");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("IO Exception occurred in PythonRunner");
-            e.printStackTrace();
-        } catch(InterruptedException e){
-            System.out.println("Interrupt Exception occurred in PythonRunner");
-            e.printStackTrace();
         }
-    }
-
-    public void reduce(DataSyncWrapper dataSyncWrapper){
-
-        try {
-           for(String key: dataSyncWrapper.getMyKeyValuesMap().keySet()){
-               Process process = createProcess(pythonReducePath);
-
-               OutputStream stdin = process.getOutputStream();
-               InputStream stdout = process.getInputStream();
-
-               BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
-               BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-
-
-               for(Integer value: dataSyncWrapper.getMyKeyValuesMap().get(key)){
-                   writer.write(key+"=>"+String.valueOf(value));
-                   writer.newLine();
-               }
-               writer.close();
-               process.waitFor();
-
-               while(reader.ready()){
-                   String result = reader.readLine();
-                   // to avoid sync problem with end of the
-                   dataSyncWrapper.addToResultList(key,result);
-               }
-           }
-           dataSyncWrapper.reduceFinished();
-
-
-        } catch (Exception e) {
-            System.out.println("Exception occurred in PythonRunner");
-            e.printStackTrace();
-        }
+        dataSyncWrapper.reduceFinished();
     }
 
     private static Process createProcess(String pythonFunctionPath) throws IOException{
